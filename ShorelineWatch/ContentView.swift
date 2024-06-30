cbe252d9833de4004e29aeaa6164117fe745a97d
Juan Marino <juasmarino@gmail.com>
2024-06-30 07:29:07 -0500

    Initial Commit

---
+++ ShorelineWatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShorelineWatch
+//
+//  Created by Juan Marino on 6/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShorelineWatch/ShorelineWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShorelineWatchApp.swift
+//  ShorelineWatch
+//
+//  Created by Juan Marino on 6/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ShorelineWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
